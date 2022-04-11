773b5d46140c6dcfaee0f5fc59e1365a2c425ac5
das <das.dev@pm.me>
2022-04-11 22:19:48 +0700

    reduce proxy view

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from aiohttp import web, hdrs
 
-from views import HackerNewsProxy
+from views import proxify_request
 
 
 app = web.Application()
-app.router.add_view('/{path:.*}', HackerNewsProxy)
+app.router.add_route(hdrs.METH_ANY, '/{path:.*}', proxify_request)
 
 if __name__ == '__main__':
     web.run_app(app)
--- views.py
+++ views.py
@@ -8,30 +8,15 @@ from services import patch_html
 from settings import HACKER_NEWS_HOST
 
 
-class HackerNewsProxy(web.View):
-    async def get(self) -> web.Response:
-        async with ClientSession() as session:
-            async with session.get(self._make_url(),
-                                   headers=self.request.headers) as response:
-                origin_content_type = response.headers.get(hdrs.CONTENT_TYPE, '')
-                origin_content = await response.content.read()
-        return self._make_response(origin_content, origin_content_type)
-
-    async def post(self) -> web.Response:
-        async with ClientSession() as session:
-            async with session.post(self._make_url(),
-                                    data=await self.request.post(),
-                                    headers=self.request.headers) as response:
-                origin_content_type = response.headers.get(hdrs.CONTENT_TYPE, '')
-                origin_content = await response.content.read()
-        return self._make_response(origin_content, origin_content_type)
-
-    def _make_url(self) -> str:
-        return urljoin(HACKER_NEWS_HOST, self.request.path_qs)
-
-    @classmethod
-    def _make_response(cls, content: bytes, content_type: str) -> web.Response:
-        if mimetypes.types_map.get('.html', '') in content_type:
-            content = patch_html(content, HACKER_NEWS_HOST)
-        headers = MultiDict({hdrs.CONTENT_TYPE: content_type})
-        return web.Response(body=content, headers=headers)
+async def proxify_request(request) -> web.Response:
+    async with ClientSession() as session:
+        async with session.request(request.method,
+                                   urljoin(HACKER_NEWS_HOST, request.path_qs),
+                                   data=await request.post(),
+                                   headers=request.headers) as response:
+            content = await response.content.read()
+            content_type = response.headers.get(hdrs.CONTENT_TYPE, '')
+            if mimetypes.types_map.get('.html', '') in content_type:
+                content = patch_html(content, HACKER_NEWS_HOST)
+            headers = MultiDict({hdrs.CONTENT_TYPE: content_type})
+            return web.Response(body=content, headers=headers)
